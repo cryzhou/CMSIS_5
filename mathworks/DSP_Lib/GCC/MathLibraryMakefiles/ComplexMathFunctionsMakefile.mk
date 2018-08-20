37c1f4ac757912f88f20f0c01d600214c0f4ea17
Raghavendra Kandur <40785458+mw-rkandur@users.noreply.github.com>
2018-08-20 09:24:02 +0530

    Update ComplexMathFunctionsMakefile.mk

--- mathworks/DSP_Lib/GCC/MathLibraryMakefiles/ComplexMathFunctionsMakefile.mk
+++ mathworks/DSP_Lib/GCC/MathLibraryMakefiles/ComplexMathFunctionsMakefile.mk
@@ -25,11 +25,10 @@ ALL_L_SRCS := $(wildcard $(MathLibraryDir)/*.cpp)
 CPPOBJS := $(patsubst %.cpp,%.cpp.o,$(ALL_L_SRCS))
 ALL_SRCS += $(ALL_L_SRCS)
 
-# Find all cpp files
-ALL_L_SRCS := $(wildcard $(MathLibraryDir)/*.s)
-ASOBJS := $(patsubst %.s,%.s.o,$(ALL_L_SRCS))
+# Find all assembly files
+ALL_L_SRCS := $(wildcard $(MathLibraryDir)/*.S)
+ASOBJS := $(patsubst %.S,%.S.o,$(ALL_L_SRCS))
 ALL_SRCS += $(ALL_L_SRCS)
-
 
 ALL_OBJS += $(addprefix $(OUTPUT_O_DIR)/,$(notdir $(COBJS))) $(addprefix $(OUTPUT_O_DIR)/,$(notdir $(CPPOBJS))) $(addprefix $(OUTPUT_O_DIR)/,$(notdir $(ASOBJS)))
 
